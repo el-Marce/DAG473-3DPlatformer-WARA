04fc4fc2747dbf41f0631bf8df2fe88e7b2ddc2b
pabloUSYIT <Phax.rod@gmail.com>
2025-10-20 17:15:21 -0400

    Agregada Wara
    - Agregada Wara
    - Instalada IA
    - Creados scripts perro

---
+++ Assets/Scripts/Dog/DogMovement.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMovement : MonoBehaviour
+{
+    //Implementar mediante navmeshsurface, para poder soportar saltos, y otros obstaculos
+}
