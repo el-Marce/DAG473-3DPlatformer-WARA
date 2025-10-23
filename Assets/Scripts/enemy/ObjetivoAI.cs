936496eb604998809b72c6e7fbee0702f99b817a
Enzoete <enzotancara@gmail.com>
2025-10-23 14:41:06 -0300

    Enemigo con navmesh
    se agrego al enemigo con un navmesh para que hace que camine porr un area delimitada, y un pequeño script para que persiga al jugador

---
+++ Assets/Scripts/enemy/ObjetivoAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ObjetivoAI : MonoBehaviour
+{
+    public Transform Target;
+    public float DistanciaAtaque;
+    private NavMeshAgent m_Agent;
+    private float m_Distancia;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Distancia = Vector3.Distance(m_Agent.transform.position, Target.position);
+        if (m_Distancia < DistanciaAtaque)
+        {
+            m_Agent.isStopped = true;
+            Debug.Log("EnemigoAtaco.");
+        }
+        else
+        {
+            m_Agent.isStopped = false;
+            m_Agent.destination = Target.position;
+        }
+    }
+}
